da03e0da25ef3723811e67f5b8668f772a11b1b9
Mathieu <mathieu.chantot@efrei.net>
2023-03-30 12:01:00 +0200

    rename

--- Projet/bufferD.vhd
+++ Projet/bufferD.vhd
@@ -7,28 +7,21 @@ entity bufferNbits is
         N : integer := 4
         );
     port(
-        e1 : in std_logic_vector (N-1 downto 0);
+        e : in std_logic_vector (N-1 downto 0);
         reset : in std_logic;
-        preset : in std_logic;
         clock : in std_logic;
-        s1 : out std_logic_vector (N-1 downto 0)
+        s : out std_logic_vector (N-1 downto 0)
     );
-end bufferNbits;
+end buffer;
 
-architecture bufferNbits_Arch of bufferNbits is
+architecture buffer_Arch of buffer is
     begin
         MyBufferNbitsProc : process (reset, clock)
         begin
             if (reset = '1') then
-                s1 <= (others => '0');
+                s <= (others => '0');
             elsif (rising_edge(clock)) then
-                if (preset = '1') then
-                --report "Presetting bufferNbits";
-                    s1 <= (others => '1');
-                else
-                --report "Buffering e1 to s1 in bufferNbit";
-                    s1 <= e1;
-                end if;
+                s <= e;
             end if;
         end process;
-end bufferNbits_Arch;
+end buffer_Arch;
